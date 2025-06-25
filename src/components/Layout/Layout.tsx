a6c43d189ace1a8bb5ad4af92fec5d24db01d548
Rom <86859467+Muharom433@users.noreply.github.com>
2025-06-26 01:28:20 +0700

    Updated Layout.tsx

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -18,14 +18,9 @@ const Layout: React.FC = () => {
   };
 
   return (
-    <div className="flex h-screen bg-gray-50 overflow-hidden">
-      {/* Sidebar */}
-      <div className={`
-        flex-shrink-0 transition-all duration-300 ease-in-out
-        ${sidebarOpen ? 'w-80' : 'w-0 lg:w-80'}
-        lg:relative lg:translate-x-0
-        ${sidebarOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'}
-      `}>
+    <div className="flex h-screen bg-gray-50">
+      {/* Sidebar - Simple Show/Hide */}
+      <div className={`${sidebarOpen ? 'w-80' : 'w-0'} lg:w-80 transition-all duration-300 overflow-hidden`}>
         <Sidebar 
           user={user} 
           isOpen={sidebarOpen} 
@@ -33,33 +28,19 @@ const Layout: React.FC = () => {
         />
       </div>
 
-      {/* Main Content Area */}
-      <div className="flex flex-col flex-1 min-w-0 overflow-hidden">
-        {/* Header */}
-        <div className="flex-shrink-0">
-          <Header
-            user={user}
-            onMenuClick={handleMenuClick}
-            onSignOut={signOut}
-            onSignIn={signIn}
-          />
-        </div>
-
-        {/* Page Content */}
-        <main className="flex-1 overflow-y-auto overflow-x-hidden bg-gradient-to-br from-gray-50 to-blue-50">
-          <div className="h-full">
-            <Outlet />
-          </div>
+      {/* Main Content */}
+      <div className="flex flex-col flex-1">
+        <Header
+          user={user}
+          onMenuClick={handleMenuClick}
+          onSignOut={signOut}
+          onSignIn={signIn}
+        />
+        
+        <main className="flex-1 overflow-auto bg-gradient-to-br from-gray-50 to-blue-50">
+          <Outlet />
         </main>
       </div>
-
-      {/* Mobile Overlay */}
-      {sidebarOpen && (
-        <div
-          className="fixed inset-0 bg-black/50 backdrop-blur-sm z-40 lg:hidden"
-          onClick={handleCloseSidebar}
-        />
-      )}
     </div>
   );
 };
