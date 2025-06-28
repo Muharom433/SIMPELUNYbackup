14bfeb75265785626c216e96d494502e3b0fb3b7
Rom <86859467+Muharom433@users.noreply.github.com>
2025-06-28 20:49:46 +0700

    Updated BookRoom.tsx

--- src/pages/BookRoom.tsx
+++ src/pages/BookRoom.tsx
@@ -8,7 +8,7 @@ import {
 import { supabase } from '../lib/supabase';
 import { useAuth } from '../hooks/useAuth';
 import { useLanguage } from '../contexts/LanguageContext';
-import {Toast} from '../components/Toast/ToastHelper';
+import {ToastHelper} from '../components/Toast/ToastHelper';
 import { Room, Department, LectureSchedule, Equipment, StudyProgram } from '../types';
 import toast from 'react-hot-toast';
 import { format, addMinutes, parse } from 'date-fns';
