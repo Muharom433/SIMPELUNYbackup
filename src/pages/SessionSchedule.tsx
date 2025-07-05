44997787645bf27be2c01a07ed5ea6e966df2fd1
Rom <86859467+Muharom433@users.noreply.github.com>
2025-07-05 13:35:40 +0700

    Updated SessionSchedule.tsx

--- src/pages/SessionSchedule.tsx
+++ src/pages/SessionSchedule.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
+import Select from 'react-select';
 import {
   Calendar,
   Search,
